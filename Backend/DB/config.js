06d707a1995d87b941d2369ab4ecd686b7a48da2
Avaneesh <avaneesh.m.maurya@gmail.com>
2025-06-22 14:39:37 +0530

    changes

--- Backend/DB/config.js
+++ Backend/DB/config.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";  
+dotenv.config(); // Load .env variables
 
-const URL = "mongodb://localhost:27017/newWorker";
+const URL = process.env.URL; // Use the URL from .env
+
+// const URL = "mongodb://localhost:27017/WorkerProject";
 
 mongoose
   .connect(URL);
